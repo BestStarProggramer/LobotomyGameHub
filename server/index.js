0fe0e4070b24d3588109e00a5c3e2bcd873a5ad7
Makzimych <mdyachekno@bk.ru>
2025-11-08 22:40:54 +0300

    feat: update index.js to use PostgreSQL instead of MySQL

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import mysql from "mysql2/promise";
+import { Pool} from "pg";
 import dotenv from "dotenv";
 import cors from "cors";
 import bcrypt from "bcrypt";
@@ -8,12 +8,10 @@ dotenv.config();
 const app = express();
 
 // --- DATABASE CONNECTION ---
-const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, // Используем строку подключения из .env
 });
+
 
 // --- MIDDLEWARES ---
 app.use(express.json());
@@ -28,3 +26,4 @@ app.use(
 app.get("/", (req, res) => {
   res.json({ message: "LobotomyGameHub API is running!" });
 });
+npm install dotenv
