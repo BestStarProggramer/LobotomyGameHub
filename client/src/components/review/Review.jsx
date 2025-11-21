d1a2324609a9d5dc8e2ee9b55be09a1561ed430a
maxim-programmer <koyushevmax@gmail.com>
2025-11-21 12:14:08 +0300

    refactor: add component review

---
+++ client/src/components/review/Review.jsx
@@ -0,0 +1,39 @@
+import "./review.scss";
+import StarIcon from "@mui/icons-material/Star";
+
+const Review = ({ review }) => {
+	const { username, avatar, rating, date, content } = review;
+
+	return (
+		<div className="review">
+			<div className="upper">
+				<div className="left_side">
+					<div className="author">
+						<img
+							src={avatar}
+							alt="аватар"
+						/>
+						<span>{username}</span>
+					</div>
+
+					<div className="rating">
+						<div className="value">
+							<StarIcon className="star" />
+							<span>{rating}</span>
+						</div>
+					</div>
+				</div>
+
+				<div className="date">
+					<p>{date}</p>
+				</div>
+			</div>
+
+			<div className="bottom_text">
+				<p>{content}</p>
+			</div>
+		</div>
+	);
+};
+
+export default Review;
