20bd350e64f86f189075b6dc6da816c0f52907b1
maxim-programmer <koyushevmax@gmail.com>
2025-11-28 23:01:57 +0300

    fix: comment out the constants from https

--- server/index.js
+++ server/index.js
@@ -10,16 +10,16 @@ import fs from "fs";
 const app = express();
 const PORT = process.env.PORT || 8800; // Используем порт из .env или 8800
 
-const key = fs.readFileSync("./localhost.key");
-const cert = fs.readFileSync("./localhost.cert");
+// const key = fs.readFileSync("./localhost.key");
+// const cert = fs.readFileSync("./localhost.cert");
 
 // --- 1. Настройка Middleware ---
 // Разрешаем Cross-Origin Resource Sharing (CORS) для фронтенда
 app.use(
-  cors({
-    origin: "http://localhost:3000", // Укажите адрес вашего клиента
-    credentials: true,
-  })
+	cors({
+		origin: "http://localhost:3000", // Укажите адрес вашего клиента
+		credentials: true,
+	})
 );
 
 // Парсер для входящих запросов в формате JSON
@@ -27,8 +27,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
+	res.header("Access-Control-Allow-Credentials", true);
+	next();
 });
 
 //подключение роутера регистрации
@@ -36,29 +36,29 @@ app.use("/api/auth", authRouter);
 
 // --- 2. API-маршрут здоровья (Health Check) ---
 app.get("/api/health", async (req, res) => {
-  try {
-    // Попытка выполнить простейший запрос к БД
-    await query("SELECT 1");
+	try {
+		// Попытка выполнить простейший запрос к БД
+		await query("SELECT 1");
 
-    // Сервер и БД работают
-    res.status(200).json({
-      status: "ok",
-      database: "connected",
-      server: `running on port ${PORT}`,
-    });
-  } catch (error) {
-    // БД недоступна
-    console.error(
-      "[Health Check] Ошибка подключения к базе данных:",
-      error.message
-    );
-    res.status(503).json({
-      status: "error",
-      database: "disconnected",
-      server: `running on port ${PORT}`,
-      message: "Database connection failed. Service Unavailable.",
-    });
-  }
+		// Сервер и БД работают
+		res.status(200).json({
+			status: "ok",
+			database: "connected",
+			server: `running on port ${PORT}`,
+		});
+	} catch (error) {
+		// БД недоступна
+		console.error(
+			"[Health Check] Ошибка подключения к базе данных:",
+			error.message
+		);
+		res.status(503).json({
+			status: "error",
+			database: "disconnected",
+			server: `running on port ${PORT}`,
+			message: "Database connection failed. Service Unavailable.",
+		});
+	}
 });
 
 // --- 3. Запуск сервера ---
@@ -68,7 +68,7 @@ app.get("/api/health", async (req, res) => {
 //   );
 // });
 app.listen(PORT, () => {
-  console.log(
-    `[Server] HTTP backend server is running on http://localhost:${PORT}`
-  );
+	console.log(
+		`[Server] HTTP backend server is running on http://localhost:${PORT}`
+	);
 });
