bda449e511435d674030a4f055913d4baf018f74
maxim-programmer <koyushevmax@gmail.com>
2025-12-12 09:42:51 +0300

    feat: add authorization context to the profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,13 +1,28 @@
 import "./profile.scss";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
 import ProfileHeader from "../../components/profileheader/ProfileHeader";
 
 const Profile = () => {
-  // Заглушка для данных пользователя
+  const { currentUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/login");
+    }
+  }, [currentUser, navigate]);
+
+  if (!currentUser) {
+    return null;
+  }
+
   const userData = {
     username: "5Hnet5K",
     avatar: "/img/profilePic.jpg",
     role: "Сотрудник редакции",
-    bio: "Немного играю в игры, немного пишу рецензии. Я тут с самого основания. На объективность особо не рассчитывайте, так как при оценке руководствуюсь в основном общими впечатлениями. А ну и если есть какие-то вопросы по сайту - можете писать мне, возможно я даже отвечу.",
+    bio: "Немного играю в игры, немного пишу рецензии. Я тут с самого основания. На объективность особо не рассчитывайте, так как при оценке руководствуюсь в основном общими впечатлениями. А ну и если есть какие-то вопросы по сайту - можете писать мне, возможно я даже отвечу",
     registrationDate: "13.10.2025",
     ratedGames: 777,
     favoriteGenres: [
@@ -22,7 +37,6 @@ const Profile = () => {
   return (
     <div className="profile">
       <div className="profile__container">
-        {/* Profile Header */}
         <ProfileHeader user={userData} />
       </div>
     </div>
