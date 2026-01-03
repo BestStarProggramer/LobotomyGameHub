e89fdd553b15b7188a355aa7846942b4fbeeddd6
Dmitriy <3sheepcanfly3@gmail.com>
2026-01-03 16:22:56 +0300

    fix: upload.js is working now

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,8 +1,12 @@
 import multer from "multer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
-const uploadDir = "../../client/public/upload";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const uploadDir = path.resolve(__dirname, "../../client/public/upload");
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
@@ -13,9 +17,9 @@ const storage = multer.diskStorage({
     cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + Math.round(Math.random() * 1e9);
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, uniqueSuffix + path.extname(file.originalname));
   },
 });
 
-export const upload = multer({ storage: storage });
+export const upload = multer({ storage });
