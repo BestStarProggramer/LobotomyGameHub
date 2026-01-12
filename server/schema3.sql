48cd6da0d0784c0d8176d34e633dc7e4b74383ea
Dmitriy <3sheepcanfly3@gmail.com>
2026-01-12 17:17:16 +0300

    edited DB

--- server/schema3.sql
+++ server/schema3.sql
@@ -186,6 +186,7 @@ CREATE TABLE IF NOT EXISTS publications (
   title VARCHAR(150) NOT NULL,
   content TEXT NOT NULL,
   views INTEGER DEFAULT 0,
+  likes INTEGER DEFAULT 0,
   created_at TIMESTAMPTZ DEFAULT now(),
   updated_at TIMESTAMPTZ DEFAULT now(),
   CONSTRAINT fk_publications_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
@@ -293,6 +294,7 @@ CREATE TABLE IF NOT EXISTS comments (
   publication_id BIGINT NOT NULL,
   parent_id BIGINT, -- Для вложенных комментариев
   content TEXT NOT NULL,
+  likes INTEGER DEFAULT 0,
   created_at TIMESTAMPTZ DEFAULT now(),
   updated_at TIMESTAMPTZ DEFAULT now(),
   CONSTRAINT fk_comments_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
